ff0fd746d5d2763d806431ebef41c435c81331e4
royalswe <roynilsson87@gmail.com>
2019-08-08 12:46:09 +0200

    add employee to about cms

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,12 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
-  />
-)
+const AboutPagePreview = ({ entry, widgetFor }) => {
+  const entryEmployees = entry.getIn(['data', 'employees', 'employee'])
+  const employee = entryEmployees ? entryEmployees.toJS() : []
+
+  return (
+    <AboutPageTemplate
+      title={entry.getIn(['data', 'title'])}
+      content={widgetFor('body')}
+      employees={{employee}}
+    />
+  )
+}
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
