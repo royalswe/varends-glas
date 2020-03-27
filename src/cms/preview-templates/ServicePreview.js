9d95e38612e3b55ff8303d9e42224388286fbd82
royalswe <roynilsson87@gmail.com>
2020-03-27 16:16:43 +0100

    built up services section

---
+++ src/cms/preview-templates/ServicePreview.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ServiceTemplate } from '../../templates/service'
+
+const ServicePreview = ({ entry, widgetFor }) => (
+  <ServiceTemplate
+    content={widgetFor('body')}
+    description={entry.getIn(['data', 'description'])}
+    title={entry.getIn(['data', 'title'])}
+  />
+)
+
+ServicePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default ServicePreview
