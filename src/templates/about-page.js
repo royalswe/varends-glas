5e8ed1fd2a96081da86e1b270806d0b65e2856a7
Roy <roynilsson87@gmail.com>
2020-04-13 16:01:14 +0200

    remove pricing

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -4,7 +4,6 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import WideFeatures from '../components/WideFeatures'
 import Testimonials from '../components/Testimonials'
-import Pricing from '../components/Pricing'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 export const AboutPageTemplate = ({
@@ -91,10 +90,6 @@ export const AboutPageTemplate = ({
                   })`,
                 }}
               />
-              <h2 className="has-text-weight-semibold is-size-2">
-                {pricing.heading}
-              </h2>
-              <p className="is-size-5">{pricing.description}</p>
             </div>
           </div>
         </div>
@@ -119,12 +114,7 @@ AboutPageTemplate.propTypes = {
     image3: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   }),
   testimonials: PropTypes.array,
-  fullImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  pricing: PropTypes.shape({
-    heading: PropTypes.string,
-    description: PropTypes.string,
-    plans: PropTypes.array,
-  }),
+  fullImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
 }
 
 const AboutPage = ({ data }) => {
@@ -141,7 +131,6 @@ const AboutPage = ({ data }) => {
         main={frontmatter.main}
         testimonials={frontmatter.testimonials}
         fullImage={frontmatter.full_image}
-        pricing={frontmatter.pricing}
       />
     </Layout>
   )
@@ -229,16 +218,6 @@ export const AboutPageQuery = graphql`
             fluid(maxWidth: 2048, quality: 100) {
               ...GatsbyImageSharpFluid
             }
-          }
-        }
-        pricing {
-          heading
-          description
-          plans {
-            description
-            items
-            plan
-            price
           }
         }
       }
